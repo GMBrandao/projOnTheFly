23aabca739b981f971774cbee08dab231ec4102f
maikeliani <maikel.iani31@gmail.com>
2023-05-06 15:00:05 -0300

    criadas classes na Model para endereco, DTO e Company

---
+++ projOnTheFly.Models/AddressDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace projOnTheFly.Models
+{
+    public  class AddressDTO
+    {
+        #region Propriedades
+        public int Id { get; set; }
+        [JsonProperty("pais")]
+        public string? Country { get; set; }
+        [JsonProperty("cep")]
+        public string CEP { get; set; }
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+        [JsonProperty("localidade")]
+        public string City { get; set; }
+        [JsonProperty("uf")]
+        public string State { get; set; }
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+        [JsonProperty("gia")]
+        public int Number { get; set; }
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+        #endregion
+    }
+}

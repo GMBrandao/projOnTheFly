ea969eb90a40a82ee0da39e370fddd48cdd8b01b
maikeliani <maikel.iani31@gmail.com>
2023-05-06 15:54:31 -0300

    criado services PostOfficeService e ValidatesCnpj

---
+++ projOnTheFly.Company/Config/ProjOnTheFlyCompanySettings.cs
@@ -0,0 +1,10 @@
+﻿namespace projOnTheFly.Company.Config
+{
+    public class ProjOnTheFly : IProjOnTheFlyCompanySettings
+    {
+        public string CompanyCollectionName { get ; set ; }
+        public string AddressCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set ; }
+    }
+}
---
+++ projOnTheFly.Services/PostOfficeService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using projOnTheFly.Models;
+
+namespace projOnTheFly.Services
+{
+    public class PostOfficeService
+    {
+        static readonly HttpClient endereco = new HttpClient();
+        public static async Task<AddressDTO> GetAddress(string cep)
+        {
+            try
+            {
+                HttpResponseMessage response = await PostOfficeService.endereco.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                response.EnsureSuccessStatusCode();
+                string ender = await response.Content.ReadAsStringAsync();
+                var end = JsonConvert.DeserializeObject<AddressDTO>(ender);
+                return end;
+            }
+            catch (HttpRequestException e)
+            {
+                throw;
+            }
+        }
+    }
+}

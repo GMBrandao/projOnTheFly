dd13adc3b9dc101e875f447ae35f5a7f11d29550
naiaragabriela <naigabi31@gmail.com>
2023-05-08 00:24:35 -0300

    criacao da SaleConsumer e da Sale em Models e do metodo de teste na SaleController

--- projOnTheFly.Sales.Consumer/Program.cs
+++ projOnTheFly.Sales.Consumer/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using projOnTheFly.Sale.Consumers;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+var saleConsumer = new SaleConsumer();
+using (var connection = factory.CreateConnection())
+{
+    while (true)
+    {
+        saleConsumer.Start(connection);
+    }
+}
+

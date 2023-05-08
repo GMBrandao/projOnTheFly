57181243fc7fb0711e4b76deaef5107dd73ade1e
naiaragabriela <naigabi31@gmail.com>
2023-05-07 22:52:18 -0300

    ajuste de merge para aplicacao rodar

--- projOnTheFly.Aircrafts/Services/AircraftsService.cs
+++ projOnTheFly.Aircrafts/Services/AircraftsService.cs
@@ -1,30 +1,26 @@
 ﻿using MongoDB.Driver;
 using projOnTheFly.Aircrafts.Config;
-using projOnTheFly.Company.Services;
-using projOnTheFly.Models;
 
 namespace projOnTheFly.Aircrafts.Services
 {
     public class AircraftsService
     {
-        private readonly IMongoCollection<Aircraft> _aircraft;
+        private readonly IMongoCollection<Models.Aircraft> _aircraft;
         private readonly IMongoCollection<Models.Company> _company;
         
         public AircraftsService(IprojOnTheFlyAircraftSettings aircraftSettings)
         {
             var aircraft = new MongoClient(aircraftSettings.ConnectionString);
             var database = aircraft.GetDatabase(aircraftSettings.DatabaseName);
-            _aircraft = database.GetCollection<Aircraft>(aircraftSettings.AircraftsCollectionName);
+            _aircraft = database.GetCollection<Models.Aircraft>(aircraftSettings.AircraftsCollectionName);
         }
 
-        public List<Aircraft> GetAll() => _aircraft.Find<Aircraft>(a => true).ToList();
+        public List<Models.Aircraft> GetAll() => _aircraft.Find<Models.Aircraft>(a => true).ToList();
 
-        public Aircraft Get(string rab) => _aircraft.Find<Aircraft>(a => a.Rab == rab).FirstOrDefault();
+        public Models.Aircraft Get(string rab) => _aircraft.Find<Models.Aircraft>(a => a.Rab == rab).FirstOrDefault();
         
-        public Aircraft Create(Aircraft aircraft)
+        public Models.Aircraft Create(Models.Aircraft aircraft)
         {
-            
-
             return aircraft;
         }
     }

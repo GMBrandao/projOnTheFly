11c3b97d1781a1718e7fa34ae7bf551c20853074
GMBrandao <gabrielmartbrandao@gmail.com>
2023-05-09 16:34:58 -0300

    Removed unused usings

--- projOnTheFly.Aircrafts/Controllers/AircraftsController.cs
+++ projOnTheFly.Aircrafts/Controllers/AircraftsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using projOnTheFly.Aircrafts.DTO;
 using projOnTheFly.Aircrafts.Services;
 using projOnTheFly.Company.Services;
